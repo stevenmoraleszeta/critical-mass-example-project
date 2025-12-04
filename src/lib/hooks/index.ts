94775a31e626c9ced7cccccdcc95a6e670e5935a
Steven Morales ZETA <108596403+stevenmoraleszeta@users.noreply.github.com>
2025-12-04 12:50:02 -0600

    Remove unused custom hooks and related assets
    Deleted several custom React hooks (useDebounce, useFocusTrap, useFormValidation, useLocalStorage, useToast) and their exports from the hooks index. Also removed related SVG assets and .gitkeep files, likely as part of a cleanup or refactor to remove unused code and files.

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -6,39 +6,13 @@ export type {
   DragAndDropHandlers,
 } from './useDragAndDrop';
 
-export { useLocalStorage } from './useLocalStorage';
-export type { UseLocalStorageOptions, UseLocalStorageReturn } from './useLocalStorage';
-
 export { useMediaQuery } from './useMediaQuery';
 
 export { useClickOutside } from './useClickOutside';
 export type { UseClickOutsideOptions } from './useClickOutside';
 
-export { useFocusTrap } from './useFocusTrap';
-export type { UseFocusTrapOptions } from './useFocusTrap';
-
 export { useToggle } from './useToggle';
 export type { UseToggleReturn } from './useToggle';
-
-export { useToast } from './useToast';
-export type {
-  Toast,
-  ToastVariant,
-  ToastPosition,
-  ToastSize,
-  UseToastOptions,
-  UseToastReturn,
-} from './useToast';
-
-export { useFormValidation } from './useFormValidation';
-export type {
-  ValidationRule,
-  UseFormValidationOptions,
-  UseFormValidationReturn,
-} from './useFormValidation';
-
-export { useDebounce } from './useDebounce';
-export type { UseDebounceOptions } from './useDebounce';
 
 export { useWindowSize } from './useWindowSize';
 export type { WindowSize, UseWindowSizeOptions } from './useWindowSize';
